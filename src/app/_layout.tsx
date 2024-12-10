d16481d957f453d8f261794d5e34fe5ad145ba71
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-12-10 16:58:19 -0300

    feat: ajustando statusBar do iphone

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,39 @@
 import { Stack } from "expo-router";
-import { colors } from "@/styles/theme"
+import { colors } from "@/styles/theme";
 import {
-    useFonts,
-    Rubik_400Regular, 
-    Rubik_500Medium, 
-    Rubik_600SemiBold,
-    Rubik_700Bold
-  } from  "@expo-google-fonts/rubik"
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
 
-import { Loading } from "@/components/loading"
+import { Loading } from "@/components/loading";
+import { SafeAreaView, StatusBar, View } from "react-native";
 
-export default function Layout(){
+export default function Layout() {
   const [fontsLoaded] = useFonts({
-    Rubik_400Regular, 
-    Rubik_500Medium, 
+    Rubik_400Regular,
+    Rubik_500Medium,
     Rubik_600SemiBold,
-    Rubik_700Bold
+    Rubik_700Bold,
   });
 
   if (!fontsLoaded) {
-    return <Loading/>;
+    return <Loading />;
   }
 
-  return <Stack screenOptions={{
-    headerShown: false,
-    contentStyle: {
-      backgroundColor: colors.gray[100]
-    }
-  }}/>;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.gray[100] }}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.gray[100]} />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: colors.gray[100],
+          },
+        }}
+      />
+    </SafeAreaView>
+  );
 }
